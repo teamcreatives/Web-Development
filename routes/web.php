003a82365fad443661aa4dbd2e43e2a427ce5fd8
Lewis <nwankaegholewis@gmail.com>
2023-01-23 03:05:48 -0800

    landing page

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/landingpage', [AdminController::class, 'landingPage'])->name('homepage');
+Route::get('/', [AdminController::class, 'landingPage'])->name('homepage');
 
 
 
