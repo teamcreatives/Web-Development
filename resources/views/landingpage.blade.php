112ca39b19177926e64c05e01231700c33247df3
Lewis <nwankaegholewis@gmail.com>
2023-01-23 03:46:58 -0800

    landing page

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -508,7 +508,6 @@
 
           <div class="section-title" data-aos="fade-up">
             <h2>Testimonials</h2>
-            <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
           </div>
 
           <div class="testimonials-slider swiper" data-aos="fade-up" data-aos-delay="100">
